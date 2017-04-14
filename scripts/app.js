dc620a376dfbf37047e2e895f8752e2e730e36ab
iwbolling <iwbolling@gmail.com>
2017-04-13 20:07:33 -0400

    Backbone removed, about page completed, projects page 25% done

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,28 @@
+// IMPORTS - JQUERY
+
+
 // IMPORTS - BACKBONE
 import Backbone from "../lib/backbone-min.js";
 
 // IMPORTS - COMPONENTS
 
 // IMPORTS - VIEWS
+import {AboutMeView} from "./views/view-about.js";
+
+// BACKBONE FUNCTIONS
+const AppRouter = Backbone.Router.extend({
+  initialize: function(){
+    console.log("Routing active.".toUpperCase());
+    Backbone.history.start();
+  },
+
+  routes: {
+    "" : "showAbout"
+  },
+
+  showAbout: function(){
+    console.log("Current View: about page");
+    let viewInstance = new AboutMeView();
+    viewInstance.render();
+  }
+})
